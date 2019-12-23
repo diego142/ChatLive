70eed5dc1e129a411c721326dbd7183e7dd54931
FRIAS ACOSTA DIEGO <mega-311@hotmail.com>
2019-12-23 16:18:20 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const socketIO = require('socket.io');
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//static files
+app.use(express.static(path.join(__dirname, 'chatWithSockets/src')));
+
+//start the server
+const server = app.listen(app.get('port'), () => {
+    console.log('server on port', app.get('port'));
+    
+});
+
+//websockets
+const io = socketIO(server); //necesita server inicializado
+
+io.on('connection', (socket) => {
+    console.log('new connection', socket.id);
+
+    socket.on('message', (data) => {
+	console.log(data);
+        io.sockets.emit('message', data);
+    });
+    
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data);
+        console.log(data);
+        
+        
+    });
+});
